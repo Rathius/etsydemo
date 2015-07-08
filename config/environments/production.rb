59dd6847c74bffe4b74e176eff2f2777ddc81033
Rathius <rathius@icloud.com>
2015-07-08 09:48:47 -0400

    Changed :host variable in /config/environments/production.rb to reflect actual production URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # Requird for Devise, remember to change localhost:3000 to actual application 	
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'http://etsydemorathius.herokuapp.com/' }
 end
