2d16afe196af6a8df98bc7df2e61de69f5e06695
Rathius <rathius@icloud.com>
2015-07-12 00:32:15 -0400

    Troubleshooting error message

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,6 +38,7 @@ class OrdersController < ApplicationController
 		    :currency => "usd",
 		    :source => token
 	    )
+	     'puts "Token is #{token}"'
 	    
 	    flash[:notice] = "Thanks for ordering!"
 	rescue Stripe::CardError => e
